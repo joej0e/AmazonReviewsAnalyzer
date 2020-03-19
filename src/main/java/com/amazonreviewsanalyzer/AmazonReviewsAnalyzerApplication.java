eb80fd7dd0e449b2d5fab52ccd917e57b4e6696a
Joanna <i1johanna1s@gmail.com>
2020-03-20 00:33:16 +0200

    initial commit

---
+++ src/main/java/com/amazonreviewsanalyzer/AmazonReviewsAnalyzerApplication.java
@@ -0,0 +1,13 @@
+package com.amazonreviewsanalyzer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AmazonReviewsAnalyzerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AmazonReviewsAnalyzerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/amazonreviewsanalyzer/AmazonReviewsAnalyzerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.amazonreviewsanalyzer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AmazonReviewsAnalyzerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
